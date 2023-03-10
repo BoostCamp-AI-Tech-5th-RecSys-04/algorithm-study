adfc6c4978118f5fd9a915b3e5ef2e95295df329
KDH <llsy159@naver.com>
2023-03-10 15:52:28 +0900

    [김동현] 이진 탐색 part2,3_오답_ 수정_및_내용 추가

--- src/이진탐색/part2_떡볶이_떡_만들기_python_김동현.py
+++ src/이진탐색/part2_떡볶이_떡_만들기_python_김동현.py
@@ -1,3 +1,13 @@
+"""
+접근법
+    1. 이진 탐색. -> 자를 떡의 길이를 기준으로 이진 탐색 시작 (1 ~ max(떡))
+    
+고찰
+    가장 기본적인 이진탐색 문제였다. 그럴수록, 정답을 구하는 과정에서 left, right 값 실수하지 않도록 주의하자
+"""
+
+
+
 n,m=map(int,input().split())
 
 rice=list(map(int,input().split()))
@@ -17,9 +27,9 @@ while(left<=right):
     자른 떡의 길이가 남은 떡의 길이와 같게 되었을 때, 
     right는 조건을 만족하지 않게 되므로 1칸 앞인 left가 답.
     '''
-    if(rest<=m):
+    if(rest<m):
         right=mid-1
     else:
         left=mid+1
 
-print(left)
+print(right)
